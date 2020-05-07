b8d361681b2e059f33c2483170c683e900ae1e9f
Maks Ponzel <maksym.ponzel@gmail.com>
2020-05-07 17:37:53 +0300

    registration page added

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './registration.css';
 import VoteService from '../../services/knuvote-service';
 import QueryString from 'query-string';
+import logo from './images/signup-image.jpg';
 
 export default class Registration extends  Component{
     state = {
@@ -40,20 +41,44 @@ export default class Registration extends  Component{
 
     render(){
         const {username,email,password} =this.state;
-        return(     
-            <form className = 'registration-class'
-            onSubmit = {this.onSubmit}>
-                <h3>Registration AREA</h3>
-                <input value={email} onChange = {this.onChangeEmail} type="text" className="form-control" placeholder="E-mail"  />
-                <input value={username} onChange = {this.onChangeUsername} type="text" className="form-control" placeholder="Username"  />
-                <input value={password} onChange = {this.onChangePassword} type="text" className="form-control" placeholder="Password" />
-                <button 
-                type="button" 
-                className="btn btn-primary"
-                onClick={this.onButtonClick} >
-                Registration</button>
-                 
-            </form>
+        return(   
+             <div class="main">
+                <section class="signup">
+                    <div class="container">
+                        <div class="signup-content">
+                            <div class="signup-form">
+                                <h2 class="form-title">Registration</h2>
+                                <form className = "register-form" id="register-form"
+                                onSubmit = {this.onSubmit}>
+                                    <div class="form-group">
+                                        <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                        <input value={email} onChange = {this.onChangeEmail} type="email" id="email" placeholder="Your Email"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                        <input value={username} onChange = {this.onChangeUsername} type="text" id="name" placeholder="Username" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                        <input value={password} onChange = {this.onChangePassword} type="password" id="pass" placeholder="Password" />
+                                    </div>
+                                    <button 
+                                    type="button" 
+                                    className="form-submit"
+                                    id="signup"
+                                    onClick={this.onButtonClick} >
+                                    Submit</button>
+
+                                </form>
+                            </div>
+                            <div class="signup-image">
+                                <figure><img src={logo} /></figure>
+                                <a href="/login" class="signup-image-link">I am already member</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         );
     }
     
