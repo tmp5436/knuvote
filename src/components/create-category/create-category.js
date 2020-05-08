cd496bdffceccc68410e0e515e5b476b9e438e09
Maks Ponzel <maksym.ponzel@gmail.com>
2020-05-08 15:51:23 +0300

    create-category updated

--- src/components/create-category/create-category.js
+++ src/components/create-category/create-category.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import VoteService from '../../services/knuvote-service';
 import DatePicker from "react-datepicker";
-import Editing from '../editing';
+import Edit from '../editing';
 import { BrowserRouter as Router, Route,Redirect, Switch } from 'react-router-dom';
 import "react-datepicker/dist/react-datepicker.css";
 import './create-category.css';
@@ -11,12 +11,10 @@ export default class CreateCategory extends Component{
     
     state = {
         name: '',
-        expiration_time: new Date(),
+        expiration_time: "",
         startDate: new Date(),
         form_next: false,
-        categoryId:0,
-        err: false,
-        msg: ''
+        categoryId:0
     }
 
     onChangeName =(e) =>{this.setState({name:e.target.value});};
@@ -26,14 +24,18 @@ export default class CreateCategory extends Component{
         e.preventDefault();
         this.obj.createCategory(this.state.name, this.state.expiration_time, localStorage.getItem('token')).then(
             (e)=>{
-                if(e.message === "Incorrect data" || e.message === "Access forbidden"){
-                    this.setState({msg:e.message})   
-                }
-                    else{
                     this.setState({
                         categoryId:e.id,
-                        err:true                      
-                    });}
+                       
+                    })
+                    this.setState(
+                        prevState => {
+                          return {
+                            ...prevState,
+                            form_next:true,
+                        };
+                      }
+                    );
             }
         )
     }
@@ -47,19 +49,40 @@ export default class CreateCategory extends Component{
         e.preventDefault();
     }
     render(){
-        const {name, err, msg,expiration_time,form_next,categoryId} = this.state;
-        const next =  err ? <Redirect to ="/editing"/> : msg;
+        const {name, expiration_time,form_next,categoryId} = this.state;
         return(
-            <div>
-                 <form>
-                 
-                    <input value = {name} onChange={this.onChangeName} ontype="text" class="form-control" required placeholder="Name"/>    
-                    <DatePicker selected={this.state.startDate} onChange={this.handleChange} placeholder="Date of expiration"/>
-                    <button onClick = {this.onBClick}>Add category</button>
-                    
-                    
-                 </form>
-                 {next}
+             <div class="limiter">
+                <div class="container-table100">
+                    <div class="wrap-table100">
+                        <div class="table100 ver1 m-b-110">
+            <form>
+                            <table data-vertable="ver1">
+                                <thead>
+                                    <tr class="row100 head">
+                                        <th class="column100 column1" data-column="column1">Name of category</th>
+                                        <th class="column100 column2" data-column="column2">Expiration date</th>
+                                        <th class="column100 column3" data-column="column3">Save</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="row100">
+                                        
+                                            <td class="column100 column1" data-column="column1"><input className="form-control"value = {name} onChange={this.onChangeName} ontype="text" placeholder="Name"/></td>                    
+                                            <td class="column100 column2" data-column="column2"><DatePicker selected={this.state.startDate} onChange={this.handleChange} placeholder="Date of expiration"/></td>
+                                            <td class="column100 column3" data-column="column3"><button class="btn btn-success" type="submit" onClick = {this.onBClick}>Add category</button>
+                                            <Route
+                                                exact
+                                                path="/editing"
+                                                render={() => (form_next ? (<Redirect to="/editing"/>) : 
+                                                <span>error</span>)}/></td>
+                                         
+                                    </tr>
+                                </tbody>
+                            </table>
+            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
       )
     }
